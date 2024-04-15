eade882f4d385fd8dc12ee5d901775a603441035
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-15 18:44:48 +0530

    feat: implement AuthProvider context
    implement auth context for providing the session to be used by the client components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import AuthProvider from '@/context/AuthProvider';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -21,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
