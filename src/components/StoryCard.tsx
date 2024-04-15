5cd9177a358179fe10a0702a968d3ec16996a8d6
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-16 01:37:04 +0530

    feat: 🚧story card component styles

--- src/components/StoryCard.tsx
+++ src/components/StoryCard.tsx
@@ -9,7 +9,7 @@ function StoryCard({ name, profile, src }: Story) {
         alt='avatar'
         width={30}
         height={30}
-        className='absolute left-2 top-2 z-10 w-8 rounded-full border-2 border-blue-500 bg-neutral-200 shadow-md filter transition-all duration-200 ease-in-out group-hover:brightness-[0.6] sm:w-10'
+        className='absolute left-2 top-2 z-10 w-8 rounded-full border-2 border-blue-500 bg-neutral-200 shadow-md filter transition-all duration-200 ease-in-out group-hover:brightness-[0.7] sm:w-10'
       />
       <Image
         src={src}
@@ -17,7 +17,7 @@ function StoryCard({ name, profile, src }: Story) {
         width={100}
         height={300}
         quality={90}
-        className=' top-10 z-50 h-full w-full bg-white brightness-[0.8] filter transition-all duration-200 ease-in-out group-hover:scale-[1.02] group-hover:brightness-[0.6] sm:block'
+        className=' top-10 z-50 h-full w-full bg-white brightness-[0.8] filter transition-all duration-200 ease-in-out group-hover:scale-[1.02] group-hover:brightness-[0.7] sm:block'
       />
       <span className='absolute bottom-2 left-2 hidden truncate text-sm text-neutral-200 lg:block'>
         {name}
