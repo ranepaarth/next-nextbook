6a88e59f938243ce159f9f0896bd686d338d5f4c
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-16 14:50:10 +0530

    chore: typings file

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,20 @@
+import { DocumentData } from "firebase/firestore";
+
 type Story = {
   name: string;
   src: string;
   profile: string;
 };
+
+type Contact = {
+  src: string;
+  name: string;
+  isOnline: boolean;
+};
+
+type PostsProps = {
+  posts:{
+    id:string
+    data?:DocumentData
+  }[]
+}
